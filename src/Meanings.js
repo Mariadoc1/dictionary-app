3d313de3c1acce7183819ffabc2446d029aaf52e
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2024-01-30 15:53:50 +0000

    Removed the example section from the dictionary

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Meanings.css";
+
 import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
@@ -8,8 +9,6 @@ export default function Meanings(props) {
       <h3>{props.meanings.partOfSpeech}</h3>
       <p>
         <strong>Definition:</strong> {props.meanings.definition}
-        <br />
-        <strong>Example:</strong> {props.meanings.example}
         <br />
         <Synonyms synonyms={props.meanings.synonyms} />
       </p>
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,5 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  return (
-    <section>
-      <div className="Phonetic">
-        <h3>Phonetic:</h3>
-        <p> {props.phonetic} </p>
-      </div>
-    </section>
-  );
+  return <div className="Phonetic">{props.phonetic}</div>;
 }
