2bcc9472cd8cf55f0235c4cf8dccdd0288ebafb2
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2024-01-30 15:53:23 +0000

    Updated the default keyword when the dictionary is loaded

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,42 +1,59 @@
 import React, { useState } from "react";
 import axios from "axios";
+
 import Results from "./Results.js";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState("dictionary");
   const [results, setResults] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data.phonetic);
     setResults(response.data);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     // documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "7odb1604cd5806418921397fa0t4e3ac";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          autoFocus={true}
-          placeholder="Enter a word..."
-          onChange={handleKeywordChange}
-          className="search"
-        ></input>
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              autoFocus={true}
+              placeholder="Enter a word..."
+              onChange={handleKeywordChange}
+              className="search"
+            ></input>
+          </form>
+          <div className="hint">e.g. sunset, forest, book...</div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
