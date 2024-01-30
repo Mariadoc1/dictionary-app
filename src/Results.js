4bb0a6834d92e42e071c399506ee73fffd63243c
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2024-01-30 15:54:35 +0000

    Changed the div to a section in order to seperate the nouns, verbs etc. when search results appeared

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Meanings from "./Meanings";
 import Phonetic from "./Phonetic";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
@@ -9,15 +10,14 @@ export default function Results(props) {
         <section>
           <h2>{props.results.word}</h2>
           <Phonetic phonetic={props.results.phonetic} />
-
-          {props.results.meanings.map(function (meanings, index) {
-            return (
-              <div key={index}>
-                <Meanings meanings={meanings} />
-              </div>
-            );
-          })}
         </section>
+        {props.results.meanings.map(function (meanings, index) {
+          return (
+            <section key={index}>
+              <Meanings meanings={meanings} />
+            </section>
+          );
+        })}
       </div>
     );
   } else {
