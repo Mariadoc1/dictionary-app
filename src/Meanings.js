7628d3e0e4807207abb2cc421626d9b2fab41081
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2024-01-30 14:02:14 +0000

    Simplified the code to get the phonetic
    The API only returned one phonetic so didn't need to loop an array

--- src/Meanings.js
+++ src/Meanings.js
@@ -6,19 +6,13 @@ export default function Meanings(props) {
   return (
     <div className="Meanings">
       <h3>{props.meanings.partOfSpeech}</h3>
-      {props.meanings.definition.map(function (props, index) {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition:</strong> {props.meanings.definition}
-              <br />
-              <strong>Example:</strong> {props.meanings.example}
-              <br />
-              <Synonyms synonyms={props.meanings.synonyms} />
-            </p>
-          </div>
-        );
-      })}
+      <p>
+        <strong>Definition:</strong> {props.meanings.definition}
+        <br />
+        <strong>Example:</strong> {props.meanings.example}
+        <br />
+        <Synonyms synonyms={props.meanings.synonyms} />
+      </p>
     </div>
   );
 }
